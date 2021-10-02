50333a8b0e3347951c25adcb6cfd0f6a669094d2
benman1 <auffarth@gmail.com>
2021-10-02 12:01:40 +0100

    docstrings

--- deepar/model/layers.py
+++ deepar/model/layers.py
@@ -5,11 +5,13 @@ from tensorflow.keras.layers import Layer
 
 class GaussianLayer(Layer):
     def __init__(self, output_dim, **kwargs):
+        """Init."""
         self.output_dim = output_dim
         self.kernel_1, self.kernel_2, self.bias_1, self.bias_2 = [], [], [], []
         super(GaussianLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        """Build the weights and biases."""
         n_weight_rows = input_shape[2]
         self.kernel_1 = self.add_weight(
             name="kernel_1",
@@ -38,13 +40,15 @@ class GaussianLayer(Layer):
         super(GaussianLayer, self).build(input_shape)
 
     def call(self, x):
+        """Do the layer computation."""
         output_mu = K.dot(x, self.kernel_1) + self.bias_1
         output_sig = K.dot(x, self.kernel_2) + self.bias_2
         output_sig_pos = K.log(1 + K.exp(output_sig)) + 1e-06
         return [output_mu, output_sig_pos]
 
     def compute_output_shape(self, input_shape):
-        """
-        The assumption is the output ts is always one-dimensional
+        """Calculate the output dimensions.
+
+        The assumption here is that the output ts is always one-dimensional;
         """
         return [(input_shape[0], self.output_dim), (input_shape[0], self.output_dim)]
