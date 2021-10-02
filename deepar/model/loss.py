294bd2473863a6c9674bd56fe1e42bd1ddaeb69c
benman1 <auffarth@gmail.com>
2021-10-02 12:01:28 +0100

    correct loss function

--- deepar/model/loss.py
+++ deepar/model/loss.py
@@ -1,15 +1,21 @@
+import math
 import tensorflow as tf
 
 
 def gaussian_likelihood(sigma):
+    """Likelihood as per the paper."""
+
     def gaussian_loss(y_true, y_pred):
-        return (
-            tf.reduce_mean(
-                0.5 * tf.math.log(sigma)
-                + 0.5 * tf.math.truediv(tf.math.square(y_true - y_pred), sigma)
+        """Updated from paper.
+
+        See DeepAR: Probabilistic Forecasting with Autoregressive Recurrent Networks.
+        """
+        return tf.reduce_mean(
+            tf.math.log(tf.math.sqrt(2 * math.pi))
+            + tf.math.log(sigma)
+            + tf.math.truediv(
+                tf.math.square(y_true - y_pred), 2 * tf.math.square(sigma)
             )
-            + 1e-6
-            + 6
         )
 
     return gaussian_loss
