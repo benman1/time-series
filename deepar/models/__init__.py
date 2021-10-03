cc20726e21873de237d2563397a866627a519f01
benman1 <auffarth@gmail.com>
2021-10-03 18:21:52 +0100

    new transformer model

---
+++ deepar/models/__init__.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+
+class NNModel(ABC):
+    def __init__(self):
+        super().__init__()
+
+    def net_structure(self, **kwargs):
+        pass
+
+    def instantiate_and_fit(self, **kwargs):
+        pass
