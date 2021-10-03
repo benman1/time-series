9fbde2394fbdbf6f3d5ed681acabee8b0e03eb96
benman1 <auffarth@gmail.com>
2021-10-03 19:33:06 +0100

    delegate pattern and loading

--- deepar/models/__init__.py
+++ deepar/models/__init__.py
@@ -1,7 +1,15 @@
+"""Template for models."""
 from abc import ABC
+import tensorflow as tf
 
 
 class NNModel(ABC):
+    """Model class."""
+
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+    ]
+
     def __init__(self):
         super().__init__()
 
@@ -10,3 +18,21 @@ class NNModel(ABC):
 
     def instantiate_and_fit(self, **kwargs):
         pass
+
+    @staticmethod
+    def load(filepath, custom_objects=None, compile=True):
+        from tensorflow.keras.models import load_model
+
+        return load_model(filepath, custom_objects, compile)
+
+    def __getattr__(self, name):
+        """Delegate pattern.
+
+        After https://github.com/faif/python-patterns:
+        * model.predict() instead of model.n_beats.predict()
+        * same for fit(), train_on_batch()
+        """
+        attr = getattr(self.model, name, None)
+        if attr is None:
+            return getattr(self, name)
+        return attr
