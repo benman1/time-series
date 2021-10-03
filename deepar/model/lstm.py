c71a1a2f95edfa85ab3e9178177c39c3257dcf5d
benman1 <auffarth@gmail.com>
2021-10-03 10:19:37 +0100

    run with new dataset classes (WIP)

--- deepar/model/lstm.py
+++ deepar/model/lstm.py
@@ -1,16 +1,17 @@
-import math
 from functools import partial
 import logging
 from typing import Optional, Union
 
 import numpy as np
 from numpy.random import normal
+import pandas as pd
 
 from tensorflow.keras.layers import Dense, Input, LSTM
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 from tensorflow.keras import callbacks
 
+from deepar.dataset.time_series import WindowGenerator
 from deepar.model.loss import gaussian_likelihood
 from deepar.model import NNModel
 from deepar.model.layers import GaussianLayer
@@ -22,8 +23,7 @@ logger = logging.getLogger(__name__)
 class DeepAR(NNModel):
     def __init__(
         self,
-        ts_obj,
-        steps_per_epoch=50,
+        ts_obj: WindowGenerator,
         epochs=100,
         loss=gaussian_likelihood,
         optimizer="adam",
@@ -33,7 +33,6 @@ class DeepAR(NNModel):
 
         self.ts_obj = ts_obj
         self.inputs, self.z_sample = None, None
-        self.steps_per_epoch = steps_per_epoch
         self.epochs = epochs
         self.loss = loss
         self.optimizer = optimizer
@@ -43,8 +42,8 @@ class DeepAR(NNModel):
         else:
             self.nn_structure = partial(
                 DeepAR.basic_structure,
-                n_steps=self.ts_obj.n_steps,
-                dimensions=self.ts_obj.dimensions,
+                n_steps=self.ts_obj.input_width,
+                dimensions=len(self.ts_obj.train_df.columns),
             )
         self._output_layer_name = "main_output"
         self.get_intermediate = None
@@ -61,11 +60,11 @@ class DeepAR(NNModel):
         input_shape = (n_steps, dimensions)
         inputs = Input(shape=input_shape)
         x = LSTM(
-            int(4 * (1 + math.pow(math.log(dimensions), 4))),
+            4,  # int(4 * (1 + math.pow(math.log(dimensions), 4))),
             return_sequences=True,
             dropout=0.1,
         )(inputs)
-        x = Dense(int(4 * (1 + math.log(dimensions))), activation="relu")(x)
+        x = Dense(4, activation="relu")(x)  # int(4 * (1 + math.log(dimensions))),
         loc, scale = GaussianLayer(dimensions, name="main_output")(x)
         return input_shape, inputs, [loc, scale]
 
@@ -86,15 +85,18 @@ class DeepAR(NNModel):
                 "auto", 0, or 1.
             patience (int): Number of epochs without without improvement to stop.
         """
+        from tensorflow.python.framework.ops import disable_eager_execution
+
+        disable_eager_execution()
+        from tensorflow.compat.v1.experimental import output_all_intermediates
+
+        output_all_intermediates(True)
+
         if not epochs:
             epochs = self.epochs
         callback = callbacks.EarlyStopping(monitor="loss", patience=patience)
         self.keras_model.fit(
-            ts_generator(self.ts_obj, self.ts_obj.n_steps),
-            steps_per_epoch=self.steps_per_epoch,
-            epochs=epochs,
-            verbose=verbose,
-            callbacks=[callback],
+            self.ts_obj.train, epochs=epochs, verbose=verbose, callbacks=[callback],
         )
         if verbose:
             logger.debug("Model was successfully trained")
@@ -107,17 +109,14 @@ class DeepAR(NNModel):
         input_shape, inputs, theta = self.nn_structure()
         model = Model(inputs, theta[0])
         model.compile(loss=self.loss(theta[1]), optimizer=self.optimizer)
+        print(model.summary())
         self.keras_model = model
 
-    def instantiate_and_fit(
-        self,
-        epochs: Optional[int] = None,
-        verbose: Union[str, int] = "auto",
-        do_fit: bool = True,
-    ):
+    def instantiate_and_fit(self, do_fit: bool = True, **fit_kwargs):
         """Compile and train model."""
         self.build_model()
-        self.fit(verbose=verbose, epochs=epochs)
+        if do_fit:
+            self.fit(**fit_kwargs)
 
     @property
     def model(self):
@@ -136,27 +135,49 @@ class DeepAR(NNModel):
 
         return self.get_intermediate(input_list)
 
-    def get_sample_prediction(self, sample):
-        sample = np.array(sample).reshape(
-            (1, self.ts_obj.n_steps, self.ts_obj.dimensions)
-        )
-        output = self.predict_theta_from_input([sample])
+    def get_sample_prediction(self, sample_df: pd.DataFrame):
+        self.ts_obj.test_df = sample_df
+        sample = self.ts_obj.test
+        output = self.predict_theta_from_input(sample)
         samples = []
         for mu, sigma in zip(output[0].reshape(-1), output[1].reshape(-1)):
             sample = normal(
                 loc=mu, scale=np.sqrt(sigma), size=1
             )  # self.ts_obj.dimensions)
             samples.append(sample)
-        return np.array(samples).reshape((self.ts_obj.n_steps, self.ts_obj.dimensions))
+
+        return np.array(samples).reshape(
+            (self.ts_obj.label_width, self.ts_obj.dimensions)
+        )
 
 
-def ts_generator(ts_obj, n_steps):
-    """
-    This is a util generator function for Keras
-    :param ts_obj: a Dataset child class object that implements the 'next_batch' method
-    :param n_steps: parameter that specifies the length of the net's input tensor
-    :return:
-    """
-    while 1:
-        batch = ts_obj.next_batch(1, n_steps)
-        yield batch[0], batch[1]
+if __name__ == "__main__":
+    """For debugging."""
+    from tensorflow.python.framework.ops import disable_eager_execution
+
+    disable_eager_execution()
+    from tensorflow.compat.v1.experimental import output_all_intermediates
+
+    output_all_intermediates(True)
+
+    import requests
+
+    resp = requests.get(
+        "https://github.com/camroach87/gefcom2017data/raw/master/data/gefcom.rda",
+        allow_redirects=True,
+    )
+    open("gefcom.rda", "wb").write(resp.content)
+    import pyreadr
+
+    result = pyreadr.read_r("gefcom.rda")
+    df = result["gefcom"].pivot(index="ts", columns="zone", values="demand")
+    from sklearn.preprocessing import StandardScaler
+    import pandas as pd
+
+    train_df = pd.DataFrame(data=StandardScaler().fit_transform(df), columns=df.columns)
+
+    ts_window = WindowGenerator(
+        input_width=10, label_width=10, shift=10, train_df=train_df
+    )
+    dp_model = DeepAR(ts_window, epochs=50)
+    dp_model.instantiate_and_fit(verbose=1, epochs=500)
