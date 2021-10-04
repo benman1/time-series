cbdbe49441b03bbce121e5391ea67d610a0bcd18
benman1 <auffarth@gmail.com>
2021-10-04 22:24:51 +0100

    install_requires

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
 
+
+with open("requirements.txt") as f:
+    install_requires = [
+        req
+        for req in
+        f.read().strip().split("\n")
+        if req
+    ]
+
+print(install_requires)
 
 setup(
     name="time-series",
@@ -9,6 +17,6 @@ setup(
     description="Time-Series models with keras and Tensorflow",
     author="Ben Auffarth",
     url="https://github.com/benman1/time-series/tree/master",
-    requires=parse_requirements("requirements.txt", session=PipSession()),
+    install_requires=install_requires,
     packages=find_packages(),
 )
