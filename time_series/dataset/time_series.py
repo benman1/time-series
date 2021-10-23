a5a7859abbda04e1fe005010756468239134c4ac
benman1 <auffarth@gmail.com>
2021-10-23 15:29:54 +0100

    remove numpy 1.20.0 dependence

--- time_series/dataset/time_series.py
+++ time_series/dataset/time_series.py
@@ -5,8 +5,14 @@ import logging
 from typing import Optional, Sequence
 
 import numpy as np
-from numpy import typing as np_types
 import pandas as pd
+
+if np.__version__ >= 1.20:
+    from numpy.typing.np_types import ArrayLike
+else:
+    from typing import Union
+    ArrayLike = Union[np.ndarray, pd.Series, pd.DataFrame]
+
 import tensorflow as tf
 from statsmodels.tsa.tsatools import lagmat
 
@@ -14,126 +20,6 @@ from time_series.dataset import Dataset
 
 
 LOGGER = logging.getLogger(__file__)
-
-
-class WindowGenerator:
-    def __init__(
-        self,
-        input_width: int,
-        label_width: int,
-        shift: int,
-        train_df=pd.DataFrame,
-        val_df=Optional[pd.DataFrame],
-        test_df=Optional[pd.DataFrame],
-        label_columns: Optional[Sequence[str]] = None,
-    ):
-        """Create a window generator for the time-series.
-
-        Adapted from: https://www.tensorflow.org/tutorials/structured_data/time_series
-
-        Parameters
-        ----------
-        input_width: how many columns
-        label_width: how many columns to predict (should be same as input_width)
-        shift: how many lags to work with
-        train_df: the dataframe, where the time-series come from. Each row should be a
-            time step, and each column is a variable that varies over time.
-        val_df: dataframe for validation.
-        test_df: dataframe for testing.
-        label_columns: this can be used for plotting.
-
-        The data will come as shape [(None, input_width, shift)]
-        """
-        # Store the raw data.
-        self.train_df = train_df
-        self.val_df = val_df
-        self.test_df = test_df
-
-        # Work out the label column indices.
-        self.label_columns = label_columns
-        if label_columns is not None:
-            self.label_columns_indices = {
-                name: i for i, name in enumerate(label_columns)
-            }
-        self.column_indices = {name: i for i, name in enumerate(train_df.columns)}
-
-        # Work out the window parameters.
-        self.input_width = input_width
-        self.label_width = label_width
-        self.shift = shift
-
-        self.total_window_size = input_width + shift
-
-        self.input_slice = slice(0, input_width)
-        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
-
-        self.label_start = self.total_window_size - self.label_width
-        self.labels_slice = slice(self.label_start, None)
-        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
-
-    def split_window(self, features):
-        inputs = features[:, self.input_slice, :]
-        labels = features[:, self.labels_slice, :]
-        if self.label_columns is not None:
-            labels = tf.stack(
-                [
-                    labels[:, :, self.column_indices[name]]
-                    for name in self.label_columns
-                ],
-                axis=-1,
-            )
-
-        # Slicing doesn't preserve static shape information, so set the shapes
-        # manually. This way the `tf.data.Datasets` are easier to inspect.
-        inputs.set_shape([None, self.input_width, None])
-        labels.set_shape([None, self.label_width, None])
-
-        return inputs, labels
-
-    def make_dataset(self, data, shuffle: bool = True):
-        data = np.array(data, dtype=np.float32)
-        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
-            data=data,
-            targets=None,
-            sequence_length=self.total_window_size,
-            sequence_stride=1,
-            shuffle=shuffle,
-            batch_size=32,
-        )
-        return ds.map(self.split_window)
-
-    def __repr__(self):
-        return "\n".join(
-            [
-                f"Total window size: {self.total_window_size}",
-                f"Input indices: {self.input_indices}",
-                f"Label indices: {self.label_indices}",
-                f"Label column name(s): {self.label_columns}",
-            ]
-        )
-
-    @property
-    def train(self):
-        return self.make_dataset(self.train_df)
-
-    @property
-    def val(self):
-        return self.make_dataset(self.val_df)
-
-    @property
-    def test(self):
-        return self.make_dataset(self.test_df, shuffle=False)
-
-    @property
-    def example(self):
-        """Get and cache an example batch of `inputs, labels` for plotting."""
-        result = getattr(self, "_example", None)
-        if result is None:
-            # No example batch was found, so get one from the `.train` dataset
-            result = next(iter(self.train))
-            # And cache it for next time
-            self._example = result
-        return result
 
 
 class TimeSeries(Dataset):
@@ -205,7 +91,7 @@ class MockTs(TimeSeries):
 
 def sample_to_input(
     sample: pd.DataFrame, lag: int, two_dim: bool = False
-) -> np_types.ArrayLike:
+) -> ArrayLike:
     """Reshape a time-series to be suitable for the models.
 
     Arguments:
@@ -241,10 +127,10 @@ class TrainingDataSet:
          lag, train_split, X_train, y_train, X_test, y_test.
     """
 
-    X_train: np_types.ArrayLike
-    y_train: np_types.ArrayLike
-    X_test: np_types.ArrayLike
-    y_test: np_types.ArrayLike
+    X_train: ArrayLike
+    y_train: ArrayLike
+    X_test: ArrayLike
+    y_test: ArrayLike
 
     def __init__(
         self,
